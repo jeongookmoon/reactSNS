602a078a42718bf939a6de0c9bd3edc51e448f49
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-06-20 23:50:30 -0700

    factored out navbar, used useCallback to optimize event handlers

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import Head from 'next/head'
-import AppLayout from '../components/AppLayout'
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>React SNS</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.js" />
-      </Head>
-      <AppLayout >
-        <div>Hello! This is index (Main page)</div>
-      </AppLayout>
+      <div>Hello! This is index (Main page)</div>
     </>
   )
 }
