beced7910e63b4490e9e892c61e98f25fedaee38
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-06-26 23:45:39 -0700

    started applying redux to react and setup for redux devtools

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,11 +2,19 @@ import React from "react"
 import Head from "next/head"
 import AppLayout from "../components/AppLayout"
 import PropTypes from "prop-types"
+// Provider provides states to React
+import { Provider } from "react-redux"
+import { createStore, compose, applyMiddleware } from "redux"
+import reducer from "../reducers"
+// to provide store state to ReactSNS
+import withRedux from "next-redux-wrapper"
 
 // using Next's component as prop, use other page as a child
-const ReactSNS = ({ Component }) => {
+const ReactSNS = ({ Component, store }) => {
   return (
-    <>
+    // since Provider is root, all children components can receive store state
+    // store contains all the reducers
+    <Provider store={store}>
       <Head>
         <title>React SNS</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.css" />
@@ -15,12 +23,23 @@ const ReactSNS = ({ Component }) => {
       <AppLayout >
         <Component />
       </AppLayout>
-    </>
+    </Provider>
   )
 }
 
 ReactSNS.propTypes = {
-  Component: PropTypes.elementType
+  Component: PropTypes.elementType,
+  store: PropTypes.object
 }
 
-export default ReactSNS
+// wrap ReactSNS to provide store states
+export default withRedux((initialState, options) => {
+  const middlewares = []
+  // to use Redux devtools on browser add the extension to middleware
+  const enhancer = compose(applyMiddleware(...middlewares),
+    // can be typeof window !== "undefined"
+    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION !== "undefined" ?
+      window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f)
+  const store = createStore(reducer, initialState, enhancer)
+  return store
+})(ReactSNS)
--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -3,11 +3,15 @@ import { Form, Input, Button } from "antd"
 import FolloingsForm from "../components/Profile/FollowingsForm"
 import FollowersForm from "../components/Profile/FollowersForm"
 
+const dummyUser = {
+  name: "Maison Margiella"
+}
+
 const Profile = () => {
   return (
     <div>
       <Form style={{ marginBottom: "20px", border: "1px solid #d9d9d9", padding: "20px" }}>
-        <Input addonBefore="Name" />
+        <Input value={dummyUser.name} addonBefore="Name" />
         <Button type="primary">Edit</Button>
       </Form>
       <FolloingsForm />
