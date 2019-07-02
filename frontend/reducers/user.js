7f96fbb7a377ba7a76285db69e79dda90f0dbb8d
Jeong Ook Moon <moonjeongook@gmail.com>
2019-07-01 23:29:30 -0700

    prepared to interact with backend

--- frontend/reducers/user.js
+++ frontend/reducers/user.js
@@ -12,44 +12,58 @@ const dummyUser = {
 }
 
 // Action Name
-export const LOG_IN = "LOG_IN"
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST"
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE"
-export const LOG_OUT = "LOG_OUT"
-export const SIGN_UP = "SIGN_UP"
+
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST"
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS"
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE"
+
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST"
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS"
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE"
+
+// For Synchronous Request, no need for saga
+export const INCREMENT_NUMBER
 
 export const loginAction = {
-  type: LOG_IN
+  type: LOG_IN_REQUEST
 }
 
 export const logoutAction = {
-  type: LOG_OUT
+  type: LOG_OUT_REQUEST
 }
 
 export const signUpAction = (data) => {
   return {
-    type: SIGN_UP,
+    type: SIGN_UP_REQUEST,
     data
   }
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOG_IN: {
+    case LOG_IN_REQUEST: {
       return {
         ...state,
-        isLoggedIn: true,
-        user: dummyUser
+        loginData: action.data
       }
     }
-    case LOG_OUT: {
+    case LOG_IN_SUCCESS: {
+      return {
+        ...state,
+        isLoggedIn: true
+      }
+    }
+    case LOG_OUT_REQUEST: {
       return {
         ...state,
         isLoggedIn: false,
         user: null
       }
     }
-    case SIGN_UP: {
+    case SIGN_UP_REQUEST: {
       return {
         ...state,
         userInfo: action.data
