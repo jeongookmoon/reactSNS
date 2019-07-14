307589ed3b2f4620db7585f460d45dd3f13bf2dc
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-07-13 22:59:57 -0700

    setup routers

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const db = require("./models")
+const userAPIRouter = require("./routes/user")
+const postAPIRouter = require("./routes/post")
+const postsAPIRouter = require("./routes/posts")
 
 const app = express()
 db.sequelize.sync()
 
-app.get("/", (request, response) => {
-  response.send("Hello, server")
-})
+app.use("/api/user", userAPIRouter)
+app.use("/api/post", postAPIRouter)
+app.use("/api/posts", postsAPIRouter)
 
 app.listen(8080, () => {
   console.log("server is running on localhost:8080")
---
+++ backend/routes/post.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/", (request, respose) => {
+
+})
+
+router.post("/images", (request, respose) => {
+
+})
+
+module.exports = router
