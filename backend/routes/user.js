38a52219de3578df91ecbfe57aeb0c11371f830c
Jeong Ook Moon <moonjeongook@gmail.com>
2019-07-13 22:59:57 -0700

    setup routers

---
+++ backend/routes/user.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (request, response) => {
+
+})
+
+router.post("/", (request, response) => {
+
+})
+
+router.get("/:id", (request, response) => {
+
+})
+
+router.post("/login", (request, response) => {
+
+})
+
+router.post("/logout", (request, response) => {
+
+})
+
+router.get("/:id/follow", (request, response) => {
+
+})
+
+router.post("/:id/follow", (request, response) => {
+
+})
+
+router.delete("/:id/follow", (request, response) => {
+
+})
+
+router.delete("/:id/follower", (request, response) => {
+
+})
+
+router.get("/:id/posts", (request, response) => {
+
+})
+
+module.exports = router
