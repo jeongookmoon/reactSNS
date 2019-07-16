727825a899c94d853375ef96a80417e97db5d7f2
Jeong Ook Moon <moonjeongook@gmail.com>
2019-07-15 23:34:06 -0700

    made register controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,34 @@
 const express = require("express")
+const bcrypt = require("bcrypt")
+const db = require("../models")
 const router = express.Router()
 
 router.get("/", (request, response) => {
 
 })
 
-router.post("/", (request, response) => {
-
+router.post("/", (request, response, next) => {
+  try {
+    const existingUser = await db.User.findOne({
+      where: {
+        userId: request.body.userId
+      }
+    })
+    if (existingUser) {
+      return response.status(403).send("User ID already exists")
+    }
+    const hashedPassword = await bcrypt.hash(request.body.password, 12)
+    const newUser = await db.User.create({
+      name: request.body.name,
+      userId: request.body.userId,
+      password: hashedPassword
+    })
+    console.log("newUser", newUser)
+    return response.status(200).json(newUser)
+  } catch (error) {
+    console.error(error)
+    return next(error)
+  }
 })
 
 router.get("/:id", (request, response) => {
