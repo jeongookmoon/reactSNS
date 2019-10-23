d416d85190f66be1ef37f5cb2207bc481c1ff1f2
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-10-22 21:31:02 -0700

    started making uploading images

--- frontend/components/Home/PostForm.js
+++ frontend/components/Home/PostForm.js
@@ -1,13 +1,14 @@
-import React, { useCallback, useState, useEffect } from "react"
+import React, { useCallback, useState, useEffect, useRef } from "react"
 import { Form, Input, Button } from "antd"
 import PropTypes from "prop-types"
 import { useSelector, useDispatch } from "react-redux"
-import { ADD_POST_REQUEST } from "../../reducers/post";
+import { ADD_POST_REQUEST, UPLOAD_IMAGES_REQUEST } from "../../reducers/post";
 
 const PostForm = ({ data }) => {
   const dispatch = useDispatch()
   const [text, setText] = useState()
   const { isAddingPost, postAdded } = useSelector(state => state.post)
+  const imageRef = useRef()
 
   useEffect(() => {
     setText("")
@@ -30,12 +31,28 @@ const PostForm = ({ data }) => {
     setText(event.target.value)
   }, [])
 
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click()
+  }, [imageInput.current])
+
+  const onChangeImage = useCallback((event) => {
+    console.log(event.target.files)
+    const imageFormData = new FormData()
+    [].forEach.call(event.target.files, (file) => {
+      imageFormData.append("image", file)
+    })
+    dispatch({
+      type: UPLOAD_IMAGES_REQUEST,
+      data: imageFormData
+    })
+  }, [])
+
   return (
     <Form encType="multipart/form-data" onSubmit={onSubmitForm}>
       <Input.TextArea maxLength={140} placeholder="What's new?" value={text} onChange={onChangeText} />
       <div style={{ marginBottom: 20 }}>
-        <input type="file" multiple hidden />
-        <Button>Image Upload</Button>
+        <input type="file" multiple hidden ref={imageInput} onChange={onChangeImage} />
+        <Button onClick={onClickImageUpload}>Image Upload</Button>
         <Button type="primary" style={{ float: "right" }} htmlType="submit" loading={isAddingPost}>Post</Button>
       </div>
       <div>
